83a798d6842d7b670ddb7664eea2058f3a9c8977
elvisplaza <elvis.plaza@hotmail.com>
2019-01-17 23:23:35 -0500

    successfully implement the api call, added a function to search particular end points and added the listing of such findings

--- src/api-call-data.js
+++ src/api-call-data.js
@@ -1,22 +1,97 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-const url = 'https://ws-api.iextrading.com/1.0/tops'
+const url = 'https://www.alphavantage.co/query'
+const apiKey= "ZJYZMYX8B8TH5QWG"
 
 class ApiCall extends Component {
   constructor(){
     super()
+      this.state={
+        company:"",
+        interval:null,
+        matches:[]
+      }
+
   }
-  componentDidMount(){
-  axios.get(url).then((results)=>{
-    console.log(results);
-  })
+
+  getUserCompany = (company)=>{
+    axios.get(`${url}`,{
+      params: {
+        function:"TIME_SERIES_DAILY",
+        symbol: `${company}`,
+        interval:"5min",
+        apikey:`${apiKey}`,
+        datatype:"json"
+      }
+    }).then((res)=>{
+      console.log(res);
+      })
+  }
+
+
+  companyMatches=(keywords)=>{
+    axios.get(`${url}`,{
+      params:{
+        function:"SYMBOL_SEARCH",
+        keywords:keywords,
+        apikey:`${apiKey}`,
+        datatype:"json"
+      }
+    }).then((res)=>{
+      let companyArray = res.data.bestMatches.map((company)=>{
+        let companyName = company["2. name"]
+        let companySymbol = company["1. symbol"]
+        
+        return {companyName,companySymbol}
+      })
+      this.setState({
+        matches:companyArray
+      })
+
+    })
+  }
+  
+
+
+
+  handleChange=(e)=>{
+    this.setState({[e.target.id]:e.target.value});
+    this.companyMatches(e.target.value)
+  }
+
+  submit=(e)=>{
+    e.preventDefault();
+    this.getUserCompany(this.state.company);
+  }
+
+  autoFill=(e)=>{
+    if(e.target.id === undefined){
+      return null
+    } else {
+      this.setState({company:e.target.id},()=>{
+        this.getUserCompany(this.state.symbol)
+      })
+    }
   }
 
   render(){
     return (
-      <div>
-        <p>  “Data provided for free by IEX. View IEX’s Terms of Use.”</p>
+      <div className="wrapper">
+        <form htmlFor="companySelection" onSubmit={this.submit}>
+          <label htmlFor="company">
+            <p>please select your company</p>
+            <input type="text" id="company" value={this.state.company} onChange={this.handleChange} />
+          </label>
+          <input type="submit"/>
+        </form>
+        <ul>
+          {this.state.matches.map((company)=>{
+            return(
+              <li onClick={this.autoFill} id={company.companySymbol} key={company.companySymbol}>{company.companyName}</li>
+            )
+          })}
+        </ul>
       </div>
     )
   }
